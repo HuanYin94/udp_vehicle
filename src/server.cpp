213cc4573587ab3aa2d49ce507bdb752bbf7cb3c
Huan Yin <zjuyinhuan@gmail.com>
2019-01-02 13:09:32 +0800

    very fast

--- src/server.cpp
+++ src/server.cpp
@@ -4,9 +4,13 @@
 #include "eigen_conversions/eigen_msg.h"
 
 #include <tf/transform_broadcaster.h>
+#include "geometry_msgs/Pose2D.h"
+#include "geometry_msgs/Twist.h"
+#include "ros/publisher.h"
 
 #include <fstream>
 #include <vector>
+#include <time.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,9 +21,14 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-#define PORT     9999
+#define PORT    9999
 #define MAXLINE 1024
 #define BYTENUM 60 // 16 for header only
+
+#define veh_length  8.800
+#define veh_width   2.394
+#define tire_radius 0.875
+
 
 using namespace std;
 
@@ -54,8 +63,13 @@ struct msg
 int main(int argc, char **argv)
 {
 
+    // INIT
     ros::init(argc, argv, "Server");
     ros::NodeHandle n;
+
+    ros::Publisher mag_pose_pub = n.advertise<geometry_msgs::Pose2D>("mag_pose", 100);
+    ros::Publisher wheel_odom_pub = n.advertise<geometry_msgs::Twist>("wheel_odom", 100);
+
 
     // SOCKET
     int sockfd;
@@ -87,9 +101,13 @@ int main(int argc, char **argv)
 
     // MSG
     msg dataFromVeh;
-    int status;
     socklen_t len;
+    geometry_msgs::Pose2D mag_pose;
+    int status;
+    geometry_msgs::Twist wheel_odom;
 
+
+    // loop closing
     while(n.ok())
     {
         cout<<"----------------------------------------------"<<endl;
@@ -98,7 +116,38 @@ int main(int argc, char **argv)
                     MSG_WAITALL, ( struct sockaddr *) &cliaddr,
                     &len);
 
-        memcpy(&dataFromVeh, buffer, BYTENUM);
+        memcpy(&dataFromVeh, buffer, sizeof(dataFromVeh));
+
+
+        // print out the message
+
+        printf("HEADER FLAG:    %x\n", dataFromVeh.Header.flag);
+        printf("HEADER TYPE:    %x\n", dataFromVeh.Header.type);
+        printf("HEADER ID:      %d\n", dataFromVeh.Header.ID);
+        printf("HEADER LEN:     %d\n", dataFromVeh.Header.length);
+        printf("HEADER SEC:     %d\n", dataFromVeh.Header.sec);
+        printf("HEADER MSEC:    %d\n", dataFromVeh.Header.miliSec);
+
+        printf("MAG POSE X:     %f\n", dataFromVeh.mag_pose_x);
+        printf("MAG POSE Y:     %f\n", dataFromVeh.mag_pose_y);
+        printf("MAG POSE H:     %f\n", dataFromVeh.mag_heading);
+
+        printf("WHEEL VEL FL:   %f\n", dataFromVeh.wheel_velocity_FL);
+        printf("WHEEL VEL FR:   %f\n", dataFromVeh.wheel_velocity_FR);
+        printf("WHEEL VEL RL:   %f\n", dataFromVeh.wheel_velocity_RL);
+        printf("WHEEL VEL RR:   %f\n", dataFromVeh.wheel_velocity_RR);
+
+        printf("WHEEL ANG FL:   %f\n", dataFromVeh.wheel_angle_FL);
+        printf("WHEEL ANG FR:   %f\n", dataFromVeh.wheel_angle_FR);
+        printf("WHEEL ANG RL:   %f\n", dataFromVeh.wheel_angle_RL);
+        printf("WHEEL ANG RR:   %f\n", dataFromVeh.wheel_angle_RR);
+
+        // publish the messages in ROS
+        mag_pose.x = dataFromVeh.mag_pose_x;
+        mag_pose.y = dataFromVeh.mag_pose_y;
+        mag_pose.theta = dataFromVeh.mag_heading;
+        mag_pose_pub.publish(mag_pose);
+
 
     }
 
